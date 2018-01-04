53e952db06aeb6f8cfb237729ef1f26b01179dcb
shanemacbride <shanemacb@gmail.com>
2018-01-04 12:26:32 -0800

    alter security groups to allow hitting apps running on nodes

--- security_groups.tf
+++ security_groups.tf
@@ -78,6 +78,20 @@ resource "aws_security_group" "nodes" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   # outbound internet access
   egress {
     from_port   = 0
