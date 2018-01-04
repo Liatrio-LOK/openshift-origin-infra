14041e41dd6f83aa56f8ac0f1e40fd5cc57106d3
shanemacbride <shanemacb@gmail.com>
2018-01-04 14:41:57 -0800

    update port 80 on node instances

--- security_groups.tf
+++ security_groups.tf
@@ -38,12 +38,13 @@ resource "aws_security_group" "master" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  /* uncomment to allow non https traffic to apps on port 80
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
+  }*/
 
   ingress {
     from_port   = 443
