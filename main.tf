ff6a3497cf38f2ac46707c999e9992152818d8c9
Shane MacBride <smacbride@mail.csuchico.edu>
2018-01-22 15:58:03 -0800

    Remote State and Deploy Scripts (#4)
    * add s3 remote state
    
    * add create script
    
    * change perms
    
    * add destruction
    
    * comment out destruction of workspace

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "openshift-origin-tfstates"
+    key = "state/os-terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
@@ -18,3 +26,12 @@ variable node_count {}
 variable github_oauth_client_id {}
 variable github_oauth_client_secret {}
 variable github_oauth_org {}
+
+data "terraform_remote_state" "network" {
+  backend = "s3"
+  config {
+    bucket = "openshift-origin-tfstates"
+    key    = "state/${terraform.workspace}/${var.cluster_prefix}-terraform.tfstate"
+    region = "us-east-1"
+  }
+}
